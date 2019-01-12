29886221241bad46ded83033d4e56998e31f99cf
Tim Arbaev <timbrk@gmail.com>
2019-01-12 23:54:37 +0300

    fix questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,11 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: [:index, :create]
+  before_action :find_question, only: [:show, :destroy]
 
   rescue_from StandardError, with: :rescue_from_question_not_found
 
   def index
-    render plain: @test.questions.pluck(:body).join("\n")
+    render inline: "<% @test.questions.each do |q| %><p>id:<%= q.id %> - <%= q.body %></p><% end %>"
   end
 
   def show
@@ -20,15 +21,19 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @test.questions[params[:id].to_i].destroy!
+    @question.destroy!
 
-    redirect_to test_questions_url
+    redirect_to test_questions_url(@question.test_id)
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
