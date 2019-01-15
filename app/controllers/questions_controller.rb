62d7ef8c0b2a338ef0ed4cd84cdcb779fd3ba687
Tim Arbaev <timbrk@gmail.com>
2019-01-15 22:32:30 +0300

    add views and test controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,46 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: [:index, :create]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy edit update]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_question_not_found
 
   def index
-    render inline: "<% @test.questions.each do |q| %><p>id:<%= q.id %> - <%= q.body %></p><% end %>"
+    redirect_to @test
   end
 
   def show
   end
 
   def new
+    @question = Question.new
+  end
+
+  def edit
+    @test = @question.test
   end
 
   def create
-    @test.questions.create!(question_params)
+    @question = @test.questions.new(question_params)
 
-    redirect_to test_questions_url
+    if @question.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question.test
+    else
+      render :edit
+    end
   end
 
   def destroy
     @question.destroy!
 
-    redirect_to test_questions_url(@question.test_id)
+    redirect_to @question.test
   end
 
   private
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,12 @@
 module QuestionsHelper
+  def question_header
+    act = params[:action] == 'new' ? 'Create' : 'Edit'
+    "#{act} question for test «#{@test.title}»"
+  end
+
+  def question_nav
+    to_test = link_to 'Back to test', test_questions_path(@test)
+    to_all_tests = link_to 'Back to all tests', tests_path
+    to_test + ' | ' + to_all_tests
+  end
 end
