dff266df0f60099fdd89288ce3bd66b4d009e2d6
Tim Arbaev <timbrk@gmail.com>
2019-01-16 11:38:30 +0300

    fix questions helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,11 +1,11 @@
 module QuestionsHelper
-  def question_header
-    act = params[:action] == 'new' ? 'Create' : 'Edit'
-    "#{act} question for test «#{@test.title}»"
+  def question_header(question)
+    act = question.new_record? ? 'Create' : 'Edit'
+    "#{act} question for test «#{question.test.title}»"
   end
 
-  def question_nav
-    to_test = link_to 'Back to test', @test
+  def question_nav(question)
+    to_test = link_to 'Back to test', test_path(question.test)
     to_all_tests = link_to 'Back to all tests', tests_path
     to_test + ' | ' + to_all_tests
   end
