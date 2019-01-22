573fb86ad7eec3b72881d88857861bc79efad33f
Tim Arbaev <timbrk@gmail.com>
2019-01-22 13:39:04 +0300

    add test_nav helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def github_url(author, repo)
     "https://github.com/#{author}/#{repo}"
   end
+
+  def test_nav(test)
+    to_test = link_to 'Back to test', test_path(test)
+    to_all_tests = link_to 'Back to all tests', tests_path
+    to_test + ' | ' + to_all_tests
+  end
 end
