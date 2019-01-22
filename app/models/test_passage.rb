1e0fcf814cd11a13b0e2ed0a82b4c2c3f9b5c551
Tim Arbaev <timbrk@gmail.com>
2019-01-22 19:28:18 +0300

    fix callback for next question

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,13 +3,10 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_next_question, on: :update
+  before_validation :before_validation_set_question
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.score += 1
-    end
+    self.score += 1 if correct_answer?(answer_ids)
     save!
   end
 
@@ -19,26 +16,22 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
-
-  def before_save_set_next_question
-    self.current_question = test.questions
-                                .order(:id)
-                                .where('id > ?', current_question.id)
-                                .first
+  def before_validation_set_question
+    self.current_question = if current_question.nil?
+                              test.questions.first
+                            else
+                              test.questions
+                                  .order(:id)
+                                  .where('id > ?', current_question.id)
+                                  .first
+                            end
   end
 
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.right_answers
-  end
-
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
