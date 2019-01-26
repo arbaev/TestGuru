26c01ec1eb5a7c4932f07de83d0b0b0dc28bf912
Tim Arbaev <timbrk@gmail.com>
2019-01-26 16:23:44 +0300

    add styles to flash-message helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   def flash_message(msg)
     if flash[msg]
-      content_tag :p, flash[msg], class: "flash-#{msg}"
+      content_tag :p, flash[msg], class: "flash flash-#{msg}"
     end
   end
 end
