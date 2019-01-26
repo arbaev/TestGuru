fa5580de5b23ebb8da6f2ee162f591e3866d15f5
Tim Arbaev <timbrk@gmail.com>
2019-01-26 15:44:09 +0300

    saving session after create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[new create]
-
   def new
     @user = User.new
   end
@@ -9,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to tests_path
     else
       render :new
