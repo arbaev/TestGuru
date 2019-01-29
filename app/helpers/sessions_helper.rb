759f3976399b0ebd48d94471b5b4b96cccec80bd
arbaev <timbrk@gmail.com>
2019-01-29 18:42:57 +0300

    change flash helper to partial
    change flash helper to partial

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,2 @@
 module SessionsHelper
-  def flash_message
-    unless flash.empty?
-      messages = flash.reduce("") do |acc, (name, msg)|
-        acc += content_tag :p, msg, class: "flash flash-#{name}"
-      end
-      messages.html_safe
-    end
-  end
 end
