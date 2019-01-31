85d085da598d51b7b24a8657d0c848a97061477e
Tim Arbaev <timbrk@gmail.com>
2019-01-31 07:37:06 +0300

    STI Admin

---
+++ db/migrate/20190131041446_add_type_and_name_attribute_to_users.rb
@@ -0,0 +1,7 @@
+class AddTypeAndNameAttributeToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :type, :string, null: false, default: 'User'
+    add_column :users, :surname, :string
+    add_index :users, :type
+  end
+end
