ff10d27a25d5c242d96c79e9a8c060f9388e24da
Tim Arbaev <timbrk@gmail.com>
2019-02-23 13:37:11 +0300

    mailgun sender email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'testguru@testguru.com'
+  config.mailer_sender = 'postmaster@sandbox239116dcbe00417abd3c3033dfd96e83.mailgun.org'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
