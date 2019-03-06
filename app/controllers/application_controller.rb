3731cebbad1567a91bb274410aeb65f33dc7f5a9
Tim Arbaev <timbrk@gmail.com>
2019-03-06 23:15:33 +0300

    delete wronf heroku configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :expire_hsts
 
   def default_url_options
     {lang: (I18n.locale unless I18n.locale == I18n.default_locale)}
@@ -15,9 +14,5 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
-  end
-
-  def expire_hsts
-    response.headers["Strict-Transport-Security"] = 'max-age=0'
   end
 end
